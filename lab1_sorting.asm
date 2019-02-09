02e4350fd23b0f0854e0055acdea1d6114d19917
siddharthBedekar <sid.bedekar@gmail.com>
2019-02-08 23:46:52 -0500

    Odd sort added to lab 1

--- lab1_sorting.asm
+++ lab1_sorting.asm
@@ -27,19 +27,40 @@ createList:
 	b createList
 
 setupInitialValues:
-	lw $t0, N  #t0 ==> iterator for beginSort; 
-	srl $t0, $t0, 1 #divide by 2
-	lw $t1, N  #t1 ==> iterator for oddSort
-	addi $t1, $t1, -2 #odd starts at N-2
-	lw $t2, N   #t2 ==> iterator for evenSort
-	addi $t2, $t2, -3 #even starts at N-3
+	lw $s0, N #s0 is the iterator for outer loop (i)
+	srl $s0, $s0, 1 # $s0 = N/2 
+
+	move $s1, $t9  #s1 ==> address iterator for evenSort
+	lw $t0,N #load N into $t0
+	addi $t0, $t0, -2 # t0 = N-2
+	move $s3, $t0 #counter j = N-2
+	sll $t0, $t0, 2  # t0 = 4*(N-2)
+	add $s1, $s1, $t0 # addr(a[j])+ 4*(N-2) = a[N-2]
+	
+	move $s2, $t9   #s2 ==> address for oddSort a[
+	lw $t0,N #load N into $t0
+	addi $t0, $t0, -3 # t0 = N-3
+	sll $t0, $t0, 2  # t0 = 4*(N-3)	
+	add $s2, $s2, $t0 #addr(a[j])+ 4*(N-3) = a[N-3]
 	b beginSort
 	  
 beginSort:
-	
-
-
+	oddSort:
+		move $t0, $s1 #t0 = address of a[j]
+		addi $t0, $t0, 4 # t0 = address of a[j+1]
+		lw $t1,0($s1) #load a[j] 
+		lw $t2,0($t0) #load a[j+1] 
+		blt $t1, $t2, noswap #jump to noswap if a[j] < a[j+1]
+		
+		#swapping
+		sw $t2, 0($s1)
+		sw $t1, 4($s1)
+	noswap:
+		addi $s1, $s1, -8 #decrement address by 2
+		addi $s3, $s3, -1 #j=j-2
+		bge $s3, $zero, oddSort
 
 
 done: 	li $v0,10	#end of code
 	syscall
+
